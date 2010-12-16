05e456927a7c5da7c043465a66d1d2796d7e3d2f
fivethreeo <oyvind.saltvik@gmail.com>
2010-12-16 18:35:10 +0100

    wrong url

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='This is a translation app/plugin with support for django-cms 2.0 plugins in the admin',
     author='Oyvind Saltvik',
     author_email='oyvind@gmail.com',
-    url='http://bitbucket.org/fivethreeo/simple-translation/',
+    url='http://github.com/fivethreeo/simple-translation/',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
