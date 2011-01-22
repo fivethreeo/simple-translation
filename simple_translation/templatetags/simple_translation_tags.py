84f013078bb4642dcdd7d05a4ae4b7c2eaf32389
fivethreeo <oyvind.saltvik@gmail.com>
2011-01-22 22:08:50 +0100

    moved filters to utils

--- simple_translation/templatetags/simple_translation_tags.py
+++ simple_translation/templatetags/simple_translation_tags.py
@@ -3,30 +3,15 @@ from django import template
 from django.template.loader import render_to_string
 from django.db import models
 from simple_translation.translation_pool import translation_pool
+from simple_translation.utils import get_preferred_translation_from_request, get_preferred_translation_from_lang
 
 register = template.Library()
 
 def annotate_with_translations(object_or_list):
     return translation_pool.annotate_with_translations(object_or_list)
 register.filter(annotate_with_translations)
-
-def get_preferred_translation_from_request(obj, request):
-    language = getattr(request, 'LANGUAGE_CODE', settings.LANGUAGE_CODE)
-    if not hasattr(obj, 'translations'):
-        annotate_with_translations(obj)
-    for translation in obj.translations:
-        if translation.language == language:
-            return translation
-    return obj.translations[0]    
+ 
 register.filter(get_preferred_translation_from_request)
-    
-def get_preferred_translation_from_lang(obj, language):
-    if not hasattr(obj, 'translations'):
-        annotate_with_translations(obj)
-    for translation in obj.translations:
-        if translation.language == language:
-            return translation
-    return obj.translations[0]
 register.filter(get_preferred_translation_from_lang)
     
 def render_language_choices(obj, request):
--- simple_translation/utils.py
+++ simple_translation/utils.py
@@ -1,4 +1,22 @@
 from django.conf import settings
+from simple_translation.translation_pool import translation_pool
 
 def get_language_from_request(request):
     return request.REQUEST.get('language', getattr(request, 'LANGUAGE_CODE', settings.LANGUAGE_CODE))
+
+def get_preferred_translation_from_request(obj, request):
+    language = getattr(request, 'LANGUAGE_CODE', settings.LANGUAGE_CODE)
+    if not hasattr(obj, 'translations'):
+        translation_pool.annotate_with_translations(obj)
+    for translation in obj.translations:
+        if translation.language == language:
+            return translation
+    return obj.translations[0]
+    
+def get_preferred_translation_from_lang(obj, language):
+    if not hasattr(obj, 'translations'):
+        translation_pool.annotate_with_translations(obj)
+    for translation in obj.translations:
+        if translation.language == language:
+            return translation
+    return obj.translations[0]
