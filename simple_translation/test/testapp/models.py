3bc251958764e24743005b182f4044acc757e06e
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2011-05-23 03:19:20 -0700

    no gettext in testapp

--- simple_translation/test/testapp/models.py
+++ simple_translation/test/testapp/models.py
@@ -2,21 +2,16 @@ import datetime
 
 from django.db import models
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
 
 class Entry(models.Model):
     is_published = models.BooleanField()
-    
-    class Meta:
-        verbose_name = _('entry')
-        verbose_name_plural = _('entries')
 
 class EntryTitle(models.Model):
-    entry = models.ForeignKey(Entry, verbose_name=_('entry'))
-    language = models.CharField(_('Language'), max_length=2, choices=settings.LANGUAGES)
-    title = models.CharField(_('Title'), max_length=255)
-    slug = models.SlugField(_('Slug'), unique=True)
-    pub_date = models.DateTimeField(_('Published'), default=datetime.datetime.now)
+    entry = models.ForeignKey(Entry)
+    language = models.CharField(max_length=2, choices=settings.LANGUAGES)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
 
     def __unicode__(self):
         return self.title
@@ -30,7 +25,3 @@ class EntryTitle(models.Model):
             'slug': self.slug
         })
     get_absolute_url = models.permalink(_get_absolute_url)
-
-    class Meta:
-        verbose_name = _('entry title')
-        verbose_name_plural = _('entry titles')
